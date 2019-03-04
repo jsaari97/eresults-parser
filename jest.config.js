0d1031dba6c0c475d37ea4bc6207cdff58f9d1d0
Jim Saari <jim.saari97@gmail.com>
2019-03-04 08:34:36 +0200

    remove junit from jest config

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: 'src',
-  reporters: ['default', 'jest-junit'],
   coverageDirectory: path.resolve(__dirname, 'coverage'),
   globals: {
     'ts-jest': {
